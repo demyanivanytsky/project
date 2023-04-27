5ae62e820c33f86684bf3d6cdf9bc1e33feefd30
Demyan <demuanivanytsky8@gmail.com>
2023-04-27 15:53:48 +0300

    script.js project

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt("Скільки фільмів ви подивились?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+ const  lastFilm1 = prompt("Останній переглянутий фільм?", ""),
+        markFilm1 = prompt("На скільки оціните фільм?", ""),
+        lastFilm2 = prompt("Останній переглянутий фільм?", ""),
+        markFilm2 = prompt("На скільки оціните фільм?", "");
+
+personalMovieDB.movies[lastFilm1] = markFilm1;
+personalMovieDB.movies[lastFilm2] = markFilm2;
+
+console.log(personalMovieDB);
