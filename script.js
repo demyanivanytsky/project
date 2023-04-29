35c60cedae9a216aa9a2cbddc76cfdf77accdec5
Demyan <demuanivanytsky8@gmail.com>
2023-04-29 16:20:16 +0300

    script.js

--- script.js
+++ script.js
@@ -14,16 +14,24 @@ for (let i = 0; i < 2; i++) {
     const lastFilm = prompt("Останній переглянутий фільм?", ""),
           markFilm = prompt("На скільки оціните фільм?", "");
  
-    if (lastFilm != null && markFilm != null && lastFilm != "" && markFilm != "" && lastFilm.length < 50) {
+    if (lastFilm != null && markFilm != null && lastFilm != "" && markFilm != "" && lastFilm.length < 50 && markFilm.length < 50) {
         personalMovieDB.movies[lastFilm] = markFilm;
         console.log("done");
     } else { 
         console.log("error");
         i--;
     }
-
-        
-   
 }
+    if (personalMovieDB.count < 10) {
+        console.log("Переглянуто мало фільмів");
+    } else if ( personalMovieDB.count>= 10 && personalMovieDB.count < 30) {
+        console.log("Ви класичний глядач");
+    } else if (personalMovieDB.count  >= 30) {
+        console.log("Ви кіноман");
+    } else { 
+        console.log("error");
+    }
+   
+
 
 console.log(personalMovieDB);
